pnplusplus/android_frameworks_base-awaken

PixelPropsUtils: Use sailfish OPM2 certified build props
Fixes play device integrity after the recent server-side update.

Change-Id: Ifa24faf41ae1f80a2ee16a92e20df0e37b008140

core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -217,18 +217,15 @@ public class PixelPropsUtils {
                 if (was) return true;
 
                 dlog("Spoofing build for GMS");
-                // Alter build parameters to pixel 2 for avoiding hardware attestation enforcement
-                setBuildField("BRAND", "google");
-                setBuildField("PRODUCT", "walleye");
-                setBuildField("MODEL", "Pixel 2");
-                setBuildField("MANUFACTURER", "Google");
-                setBuildField("DEVICE", "walleye");
-                setBuildField("FINGERPRINT", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-                setBuildField("ID", "OPM1.171019.011");
+                // Alter build parameters to pixel for avoiding hardware attestation enforcement
+                setBuildField("PRODUCT", "sailfish");
+                setBuildField("MODEL", "Pixel");
+                setBuildField("DEVICE", "sailfish");
+                setBuildField("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM2.171019.029/4657601:user/release-keys");
                 setBuildField("TYPE", "user");
                 setBuildField("TAGS", "release-keys");
                 setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.O_MR1);
-                setVersionField("SECURITY_PATCH", "2017-12-05");
+                setVersionField("SECURITY_PATCH", "2018-04-05");
                 return true;
             }
         }

inex-droid/frameworks_base

core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -250,13 +250,15 @@ public class PixelPropsUtils {
                 if (was) return true;
 
                 dlog("Spoofing build for GMS");
-                // Alter build parameters to Pixel 2 XL for avoiding hardware attestation enforcement
-                setPropValue("DEVICE", "taimen");
-                setPropValue("FINGERPRINT", "google/taimen/taimen:8.1.0/OPM4.171019.021.R1/4833808:user/release-keys");
-                setPropValue("MODEL", "Pixel 2 XL");
-                setPropValue("PRODUCT", "taimen");
-                setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N);
-                setVersionFieldString("SECURITY_PATCH", "2018-07-05");
+                // Alter build parameters to pixel for avoiding hardware attestation enforcement
+                setPropValue("PRODUCT", "sailfish");
+                setPropValue("MODEL", "Pixel");
+                setPropValue("DEVICE", "sailfish");
+                setPropValue("FINGERPRINT", "google/sailfish/sailfish:8.1.0/OPM2.171019.029/4657601:user/release-keys");
+                setPropValue("TYPE", "user");
+                setPropValue("TAGS", "release-keys");
+                setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.O_MR1);
+                setVersionFieldString("SECURITY_PATCH", "2018-04-05");
                 return true;
             }
         }